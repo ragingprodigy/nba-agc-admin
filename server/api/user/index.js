a19ebed0470b46bb335e31b928079e53b729de79
Rasheed Rahman <ra1da35ma@gmail.com>
2016-08-10 10:43:49 +0100

    Added back QR-Code to Name Tag Template

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -8,7 +8,9 @@ var router = express.Router();
 router.get('/', sessionSec, controller.index);
 router.get('/getGroups', sessionSec, controller.getGroups);
 router.get('/getTags', sessionSec, controller.getTags);
+router.get('/getRegistrationTags', sessionSec, controller.getRegistrationTags);
 router.post('/create',sessionSec, controller.create);
+router.get('/allUsersForNameTags', sessionSec, controller.allUsersForNameTags);
 router.get('/:id', sessionSec, controller.show);
 router.post('/:id/reset', sessionSec, controller.reset);
 
--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -8,16 +8,18 @@ var Invoice = require('../invoice/invoice.model');
 var ObjectId = require('mongoose').Types.ObjectId;
 
 // Get User Tags for Printing
-exports.getTags = function(req, res) {
+exports.getTags = function (req, res) {
 
-    var sendData = function(response) {
+    var sendData = function (response) {
         return res.json(response);
     };
 
-    if (typeof req.query.tags == "string") { req.query.tags = [req.query.tags]; }
+    if (typeof req.query.tags == "string") {
+        req.query.tags = [req.query.tags];
+    }
 
     // Get Users
-    User.find({_id: {$in: req.query.tags}}, function(err, usersWithTags){
+    User.find({_id: {$in: req.query.tags}}, function (err, usersWithTags) {
 
         var response = [];
 
@@ -25,53 +27,55 @@ exports.getTags = function(req, res) {
 
             console.log('Doing...Single');
             // Get Registration Info
-            Registration.findOne({user: new ObjectId(req.query.tags)}, function(err, reg){
+            Registration.findOne({user: new ObjectId(req.query.tags)}, function (err, reg) {
 
                 var u = usersWithTags[0];
 
-                response.push((u.hasTag?{
+                response.push((u.hasTag ? {
                     prefix: u.prefix,
                     suffix: u.suffix,
                     name: u.name,
                     company: u.firm,
                     id: u._id,
-                    code: reg?(reg.registrationCode):''
-                }:reg?{
+                    code: reg.registrationCode
+                    // code: reg?(reg.registrationCode):''
+                } : reg ? {
                     prefix: reg.prefix,
                     suffix: reg.suffix,
-                    name: reg.firstName+' '+ (!reg.middleName.length?'':(reg.middleName.substring(0,1)+'. '))+ reg.surname,
-                    company: reg.registrationType!='judge'&&reg.registrationType!='magistrate'?reg.company:(reg.court+' '+reg.state+' '+reg.division),
+                    name: reg.firstName + ' ' + (!reg.middleName.length ? '' : (reg.middleName.substring(0, 1) + '. ')) + reg.surname,
+                    company: reg.registrationType != 'judge' && reg.registrationType != 'magistrate' ? reg.company : (reg.court + ' ' + reg.state + ' ' + reg.division),
                     id: u._id,
                     code: reg.registrationCode
-                }:{}));
+                } : {}));
 
-                User.update({_id: {$in: req.query.tags}}, {'$set': {tagPrinted:true, lastModified:new Date()}}, function() { return; });
+                User.update({_id: {$in: req.query.tags}}, {'$set': {tagPrinted: true, lastModified: new Date()}}, function () {
+                    return;
+                });
 
                 return sendData(response);
 
             });
         } else {
             // Get Registration Info
-            Registration.find({user: {$in: req.query.tags}}, function(err, regs){
+            Registration.find({user: {$in: req.query.tags}}, function (err, regs) {
 
-                _.each(usersWithTags, function(u) {
-                    var theReg = _.find(regs, { 'user':u._id });
+                _.each(usersWithTags, function (u) {
+                    var theReg = _.find(regs, {'user': u._id});
                     //var theReg = _.find(regs, function(r) { console.log('%s == %s', r.user, u._id); return r.user== u._id; });
                     if (u.name) {
                         var name_split = u.name.split(' ');
                     }
-                    response.push((u.hasTag?{
-                        prefix: u.prefix,
-                        suffix: u.suffix,
-                        surname: name_split[0],
-                        firstName: name_split.length>2?name_split[name_split.length-1]:name_split[1],
-                        middleName: name_split.length>2?name_split[1]:'',
-                        branch: theReg.branch,
-                        // name: u.name,
-                        // company: u.firm,
-                        id: u._id,
-                        code: u?(u.registrationCode):''
-                    }:theReg?{
+                    response.push((u.hasTag ? {
+                        // prefix: u.prefix,
+                        // suffix: u.suffix,
+                        // surname: name_split[0],
+                        // firstName: name_split.length>2?name_split[name_split.length-1]:name_split[1],
+                        // middleName: name_split.length>2?name_split[1]:'',
+                        // branch: theReg.branch,
+                        // // name: u.name,
+                        // // company: u.firm,
+                        // id: u._id,
+                        // code: u?(u.registrationCode):''
                         prefix: theReg.prefix,
                         suffix: theReg.suffix,
                         surname: theReg.surname,
@@ -82,10 +86,23 @@ exports.getTags = function(req, res) {
                         // company: theReg.registrationType!='judge'&&theReg.registrationType!='magistrate'?theReg.company:(theReg.court+' '+theReg.state+' '+theReg.division),
                         id: u._id,
                         code: theReg.registrationCode
-                    }:{}));
+                    } : theReg ? {
+                        prefix: theReg.prefix,
+                        suffix: theReg.suffix,
+                        surname: theReg.surname,
+                        firstName: theReg.firstName,
+                        middleName: theReg.middleName,
+                        branch: theReg.branch,
+                        // name: theReg.firstName+' '+ (!theReg.middleName.length?'':(theReg.middleName.substring(0,1)+'. '))+ theReg.surname,
+                        // company: theReg.registrationType!='judge'&&theReg.registrationType!='magistrate'?theReg.company:(theReg.court+' '+theReg.state+' '+theReg.division),
+                        id: u._id,
+                        code: theReg.registrationCode
+                    } : {}));
                 });
 
-                User.update({_id: {$in: req.query.tags}}, {'$set': {tagPrinted:true}}, {multi:true}, function() { return; });
+                User.update({_id: {$in: req.query.tags}}, {'$set': {tagPrinted: true}}, {multi: true}, function () {
+                    return;
+                });
 
                 return sendData(response);
             });
@@ -94,12 +111,14 @@ exports.getTags = function(req, res) {
 };
 
 // Get list of users
-exports.index = function(req, res) {
+exports.index = function (req, res) {
     if (req.query.unregistered) {
-        Registration.find({ completed: true, isGroup:false }).select('email').exec(function(err, registered){
-            var mArr = _.uniq(_.map(registered, function(r){ return r.email.toLowerCase(); }));
+        Registration.find({completed: true, isGroup: false}).select('email').exec(function (err, registered) {
+            var mArr = _.uniq(_.map(registered, function (r) {
+                return r.email.toLowerCase();
+            }));
 
-            User.find({email: { $nin: mArr, $regex: new RegExp('@', 'i') }, accountType: "single" }).sort('lastModified').select('email lastModified _id').exec(function(err, users) {
+            User.find({email: {$nin: mArr, $regex: new RegExp('@', 'i')}, accountType: "single"}).sort('lastModified').select('email lastModified _id').exec(function (err, users) {
                 res.json(users);
 
             });
@@ -128,20 +147,28 @@ exports.index = function(req, res) {
         } else if (req.query.page || req.query.perPage) {
             var page = (req.query.page || 1) - 1,
                 perPage = req.query.perPage || 25;
-            User.find({}).exec( function (err, users) {
-                if(err) { return handleError(res, err); }
+            User.find({}).exec(function (err, users) {
+                if (err) {
+                    return handleError(res, err);
+                }
 
-                if(err) { return handleError(res, err); }
+                if (err) {
+                    return handleError(res, err);
+                }
                 // Return Only Users with Confirmed Registration Data
                 var userIds = _.pluck(users, '_id');
 
-                Registration.count({user:{$in: userIds}, paymentSuccessful:true, statusConfirmed:true}, function (e, total) {
-                    Registration.find({user:{$in: userIds}, paymentSuccessful:true, statusConfirmed:true}).select('_id prefix suffix surname middleName branch firstName registrationCode user')
+                Registration.count({user: {$in: userIds}, paymentSuccessful: true, statusConfirmed: true}, function (e, total) {
+                    Registration.find({
+                        user: {$in: userIds},
+                        paymentSuccessful: true,
+                        statusConfirmed: true
+                    }).select('_id prefix suffix surname middleName branch firstName registrationCode user')
                         .populate('user')
                         .sort('branch')
                         .skip(page * perPage)
                         .limit(perPage)
-                        .exec(function(err, registrations){
+                        .exec(function (err, registrations) {
                             var _toReturn = _.pluck(registrations, 'user');
                             res.header('total_found', total);
                             return res.json(registrations);
@@ -151,28 +178,33 @@ exports.index = function(req, res) {
 
         } else {
             User.find(req.query, function (err, users) {
-                if(err) { return handleError(res, err); }
+                if (err) {
+                    return handleError(res, err);
+                }
 
-                if(err) { return handleError(res, err); }
+                if (err) {
+                    return handleError(res, err);
+                }
                 // Return Only Users with Confirmed Registration Data
                 var userIds = _.pluck(users, '_id');
 
-                Registration.find({user:{$in: userIds}, paymentSuccessful:true, statusConfirmed:true}).select('_id, surname, middleName, firstName, user')
-                    .populate('user').exec(function(err, registrations){
-                        var _toReturn = _.pluck(registrations, 'user');
-                        return res.json(_toReturn);
-                    });
+                Registration.find({user: {$in: userIds}, paymentSuccessful: true, statusConfirmed: true}).select('_id, surname, middleName, firstName, user')
+                    .populate('user').exec(function (err, registrations) {
+                    var _toReturn = _.pluck(registrations, 'user');
+                    return res.json(_toReturn);
+                });
             });
         }
     }
 };
 
-exports.reset = function(req, res) {
+// ---
+exports.reset = function (req, res) {
 
-    User.findById(req.params.id, '+password', function(err, existingUser) {
+    User.findById(req.params.id, '+password', function (err, existingUser) {
 
         existingUser.password = existingUser.generateHash('1234');
-        existingUser.save(function() {
+        existingUser.save(function () {
             delete existingUser.password;
             res.json(existingUser);
 
@@ -181,49 +213,107 @@ exports.reset = function(req, res) {
 };
 
 // Get a single user
-exports.show = function(req, res) {
-  User.findById(req.params.id, function (err, user) {
-    if(err) { return handleError(res, err); }
-    if(!user) { return res.send(404); }
-    return res.json(user);
-  });
+exports.show = function (req, res) {
+    User.findById(req.params.id, function (err, user) {
+        if (err) {
+            return handleError(res, err);
+        }
+        if (!user) {
+            return res.send(404);
+        }
+        return res.json(user);
+    });
 };
 
 //get manually created Groups
-exports.getGroups = function(req, res) {
-  User.find(req.query, function (err, user) {
-    if(err) { return handleError(res, err); }
-    if(!user) { return res.send(404); }
-    return res.json(user);
-  });
+exports.getGroups = function (req, res) {
+    User.find(req.query, function (err, user) {
+        if (err) {
+            return handleError(res, err);
+        }
+        if (!user) {
+            return res.send(404);
+        }
+        return res.json(user);
+    });
 };
 
 // Creates a new user in the DB.
-exports.create = function(req, res) {
-  var email = new RegExp(req.body.email,'i');
-  User.findOne({email:email}, function (err,foundUser) {
-    if (foundUser){
-      return res.json(200,{message:'Email is already Taken Please Try Another Email.'})
-    }
-    var user = new User();
+exports.create = function (req, res) {
+    var email = new RegExp(req.body.email, 'i');
+    User.findOne({email: email}, function (err, foundUser) {
+        if (foundUser) {
+            return res.json(200, {message: 'Email is already Taken Please Try Another Email.'})
+        }
+        var user = new User();
 
-    user.email = req.body.email;
-    user.groupName = req.body.groupName;
-    user.password = user.generateHash(req.body.password);
-    user.phone = req.body.phone;
-    user.accountType = 'group';
-    user.isDirect = true;
+        user.email = req.body.email;
+        user.groupName = req.body.groupName;
+        user.password = user.generateHash(req.body.password);
+        user.phone = req.body.phone;
+        user.accountType = 'group';
+        user.isDirect = true;
 
-    user.save(function(err, user) {
-      delete user.password;
-      return res.json(200,user);
+        user.save(function (err, user) {
+            delete user.password;
+            return res.json(200, user);
+
+        });
 
     });
 
-  });
+};
 
+// Get list of users for NAMETAG printing
+exports.allUsersForNameTags = function (req, res) {
+    if (req.query.page || req.query.perPage) {
+        var page = (req.query.page || 1) - 1,
+            perPage = req.query.perPage || 25;
+
+        Registration.count({paymentSuccessful: true, statusConfirmed: true}, function (e, total) {
+            Registration.find({
+                // user: {$in: userIds},
+                paymentSuccessful: true,
+                statusConfirmed: true
+            }).select('_id prefix suffix surname middleName branch firstName registrationCode tagPrinted user')
+                .populate('user')
+                .sort('registrationCode')
+                .skip(page * perPage)
+                .limit(perPage)
+                .exec(function (err, registrations) {
+                    var _toReturn = _.pluck(registrations, 'user');
+                    res.header('total_found', total);
+                    return res.json(registrations);
+                });
+        });
+
+    } else {
+
+    }
+};
+
+// Get Registration Tags for Printing
+exports.getRegistrationTags = function (req, res) {
+
+    var sendData = function (response) {
+        return res.json(response);
+    };
+
+    if (typeof req.query.tags == "string") {
+        req.query.tags = [req.query.tags];
+    }
+
+    // Get Registrations
+    Registration.find({_id: {$in: req.query.tags}}, 'prefix suffix surname firstName middleNmae branch registrationCode', function (err, regs) {
+
+        Registration.update({_id: {$in: req.query.tags}}, {'$set': {tagPrinted: true}}, {multi: true}, function () {
+            return;
+        });
+
+        return sendData(regs);
+    });
 };
 
 function handleError(res, err) {
-  return res.send(500, err);
+    return res.send(500, err);
 }
